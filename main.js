14c54d65fd1bec1f1a467632951fe9850a1cfab8
Thiago <tmenezessilva34@gmail.com>
2022-10-22 16:05:32 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// uma função  que bisca os testo <h1> e <h2>
+
+//função vai receber uma  texto e vou procurar coisa similares
+
+
+let nome = ""
+
+function  buscar (nome){
+   // return  document.getElementsByName("hello").innerHTML;
+return  document.getElementByName("hello").textContent;
+}
+document.getElementById("texto").textContent
+
+console(document.getElementByName("hello").textContent);
+
+
+
